1914ea42375effb92ea3cb82653ef67692f45d24
vicSystems <victorasuquob@gmail.com>
2023-02-11 10:15:15 +0100

    configure redirects

---
+++ src/views/InvoiceView.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="container">
+            <img alt="Vue logo" style="height: 50px;" class="text-center" src="../assets/logo.png">
+
+            <h3 class="py-5">Vicsystems Technologies Ltd.</h3>
+        </div>
+
+        <div class="container mt-5">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Unit Price</th>
+                        <th>QTY</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    //
+                    <tr>
+                        <td>1</td>
+                        <td>UCC 828Liters Freezer</td>
+                        <td>ULtra low temp. -86 Degrees</td>
+                        <td>NGN 800,000.00</td>
+                        <td>2</td>
+                        <td>NGN 1, 600, 000</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            invoiceItems: []
+        }
+    },
+}
+</script>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="../assets/logo.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            products: []
+        }
+    },
+}
+</script>
