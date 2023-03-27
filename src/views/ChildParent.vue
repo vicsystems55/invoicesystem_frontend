c83404f492dca1be575fc32841f915a3e040c788
vicSystems <victorasuquob@gmail.com>
2023-03-27 14:16:05 +0100

    online payment

---
+++ src/views/ChildParent.vue
@@ -0,0 +1,22 @@
+<!-- ChildComponent.vue -->
+<template>
+    <div>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message: 'Hello from child component!',
+      };
+    },
+    methods: {
+      childFunction() {
+        this.message = 'Child component function was called!';
+      },
+    },
+  };
+  </script>
+  
---
+++ src/views/ParentChild.vue
@@ -0,0 +1,23 @@
+<!-- ParentComponent.vue -->
+<template>
+    <div>
+      <button @click="childComponentFunction">Call child component function</button>
+      <ChildComponent ref="childComponentRef" />
+    </div>
+  </template>
+  
+  <script>
+  import ChildComponent from './ChildComponent.vue';
+  
+  export default {
+    components: {
+      ChildComponent,
+    },
+    methods: {
+      childComponentFunction() {
+        this.$refs.childComponentRef.childFunction();
+      },
+    },
+  };
+  </script>
+  
